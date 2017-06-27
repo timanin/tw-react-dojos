a00ec505742adc1dbedd7e15d06c52f10b140889
Sergey Timanin <sergey.timanin@clarivate.com>
2017-06-27 20:09:27 +0200

    End of first dojo

--- first/dojo/src/App.js
+++ first/dojo/src/App.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import {Astronaut} from './Astronaut.js';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            loaded: false,
+            error: null,
+            number: 0,
+            people: []
+        };
+    }
+
+    componentWillMount() {
+        fetch('http://api.open-notify.org/astros.json')
+            .then(response => response.json())
+            .then(result => this.setState({...this.state, loaded: true, ...result}))
+            .catch(error => this.setState({...this.state, error: true}));
+    }
+
+    render() {
+        console.log(this.state);
+        const { number, people, loaded, error } = this.state;
+        return (
+            <div className="App">
+              <h1>How many people are in space?</h1>
+              {!loaded && !error && <span>Looking at the sky!</span>}
+              {error && <span>Can't see the sky!'</span>}
+              {loaded && !error &&
+                  <div>
+                      <span>{number}</span>
+                          <ul>
+                                {people.map(astronaut => (<Astronaut
+                                                                key={astronaut.name}
+                                                                name={astronaut.name}
+                                                                craft={astronaut.craft}
+                                                                />))}
+                          </ul>
+                 </div>
+              }
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ first/dojo/src/Astronaut.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Astronaut = props =>
+    <li>{props.craft} - {props.name}</li>;
+
